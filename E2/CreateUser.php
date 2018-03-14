f70f2e678f7ac6d06134d0ad4843b8beac17f720
SiluoFeng <s682f720@ku.edu>
2018-03-14 09:59:52 -0500

    Add E2

---
+++ E2/CreateUser.php
@@ -0,0 +1,23 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "s682f720", "aX7caeyi", "s682f720");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "INSERT INTO Users (user_id) VALUES ('$username')";
+
+if($username == ""){
+  echo "Wrong!Username cannot blank <br>";
+}else{
+  if($mysqli->query($query)){
+    echo "Submit successfully";
+  }else{
+    echo "Wrong!Users already in database!";
+  }
+}
+/* close connection */
+$mysqli->close();
+?>
